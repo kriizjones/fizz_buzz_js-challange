806a42cb24a40560ba2bd331fbb9e496088d0240
kriizjones <kristoffer.s.karlsson@hotmail.com>
2016-11-27 20:17:07 +0100

    fizz,buzz and fizzbuzz come to  be numbers

---
+++ spec/fizz_buzz_spec.js
@@ -0,0 +1,19 @@
+describe("fizzBuzzer", function() {
+
+it("should be one", function() {
+  expect(fizzBuzzer(1)).toEqual(1);
+});
+
+it("should be fizz", function() {
+  expect(fizzBuzzer(3)).toEqual('fizz');
+});
+
+it("should be buzz", function() {
+  expect(fizzBuzzer(5)).toEqual('buzz');
+});
+
+it("should be fizzbuzz", function() {
+  expect(fizzBuzzer(15)).toEqual('fizzbuzz');
+});
+
+});
